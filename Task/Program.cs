5921eb58ae7841085b47113f0f6d5a47e35751d3
Svetlana <sezga@rambler.ru>
2022-10-05 23:48:23 +0300

    Написали функцию для перебора строк, нахождения строк где символов менее или равно 3 и запись в новый массив

--- Task/Program.cs
+++ Task/Program.cs
@@ -7,3 +7,18 @@
     }
 }
 
+
+int FillArrayLength3(int iCount, string[] array, ref string[] array2)
+{
+    int n = 0;
+    for (int i = 0; i  < iCount; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[n] = array[i];
+            n++;
+        }
+    }
+    Array.Resize(ref array2, n);
+    return n;
+}
