e586c5d024cd49bd17cbb04b606fdea7ce9e224b
Svetlana <sezga@rambler.ru>
2022-10-05 23:45:09 +0300

    Написали функцию для заполнения массива от пользователя

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int iCount, string[] array)
+{
+    for (int i = 0; i  < iCount; i++)
+    {
+        Console.WriteLine($"Введите строку {i+1}: ");
+        array[i] = Console.ReadLine() ?? string.Empty;
+    }
+}
+
